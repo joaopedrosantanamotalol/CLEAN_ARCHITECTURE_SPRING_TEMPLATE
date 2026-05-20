aaba5a1d0e58783dfd173960aeb408ec16f9bfd0
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-05-19 21:55:12 -0300

    subindo projeto

--- src/main/java/com/cleantemplate/base/Application.java
+++ src/main/java/com/cleantemplate/base/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RmaprotocolApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RmaprotocolApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
